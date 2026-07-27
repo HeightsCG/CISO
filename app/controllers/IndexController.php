07a0172e02da0202471a6240168dae106ebc2266
Dr. Daniel Glauber <dan.glauber@oliverwyman.com>
2026-07-27 14:16:12 -0400

    Create IndexController.php

---
+++ app/controllers/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+class IndexController extends Controller {
+
+    public $protected = 1;
+
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function indexAction(){
+        $this->view->render();
+    }
+
+}
