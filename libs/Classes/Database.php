b3d6974a93658b7a6ed744ac38bad720d592ea87
Dr. Daniel Glauber <dan.glauber@oliverwyman.com>
2026-07-29 16:03:23 -0400

    Update Database.php

--- libs/Classes/Database.php
+++ libs/Classes/Database.php
@@ -4,6 +4,7 @@ class Database extends PDO {
     public function __construct() {
         $env     = Main::get_environment();
         $config  = Main::get_config();
+        print_r($config);
         $db_type = $config[$env]['db_type'];
         $db_name = $config[$env]['db_name'];
         $db_user = $config[$env]['db_user'];
